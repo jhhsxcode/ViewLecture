c9c8ae725b9b7fe4ab9456e7ecdbb5fd01931bd5
Bob Brown <bob.brown@d214.org>
2020-11-06 10:46:53 -0600

    Initial Commit

---
+++ ViewLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewLecture
+//
+//  Created by Robert D. Brown on 11/6/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
